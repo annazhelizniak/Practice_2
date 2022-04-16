077fe67ceeab42845217d1906ba30cdb373a0957
Zhelizniak Anna <anna.zhelizniak@ukma.edu.ua>
2022-04-16 20:21:21 +0300

    Async

---
+++ Models/Person.cs
@@ -0,0 +1,185 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Practice_2.Models
+{
+    //class user contains field which saves date of birth and methods which count age, chinese and western zodiac signs
+    class Person
+    {
+        #region Fields
+
+        private string _name;
+        private string _surname;
+        private DateTime _dateOfBirth = DateTime.Now;
+        private string _email;
+        #endregion
+
+        #region Constructors
+
+        public Person(string name, string surname, string email)
+        {
+            _name = name;
+            _surname = surname;
+            _email = email;
+
+        }
+
+        public Person(string name, string surname, DateTime dateOfBirth)
+        {
+            _name = name;
+            _surname = surname;
+            _dateOfBirth = dateOfBirth;
+        }
+
+        public Person(string name, string surname, DateTime dateOfBirth, string email):this(name,surname,dateOfBirth)
+        {
+            _email = email;
+        }
+
+        public Person()
+        {
+
+        }
+
+
+        #endregion
+        #region Properties
+
+        public string Name
+        {
+            get { return _name; }
+            set { _name = value; }
+        }
+
+        public string Email
+        {
+            get { return _email; }
+            set { _email = value; }
+        }
+
+        public DateTime DateOfBirth
+        {
+            get { return _dateOfBirth; }
+            set { _dateOfBirth = value; }
+        }
+
+        public string Surname
+        {
+            get { return _surname; }
+            set { _surname = value; }
+        }
+
+        public bool IsAdult
+        {
+            get
+            {
+                if (Age() < 18) return false;
+                return true;
+            }
+        }
+
+        public string SunSign
+        {
+            //find out western zodiac sign
+            get
+            {
+                string sign = null;
+                switch (_dateOfBirth.Month)
+                {
+                    case 1:
+                        sign = (_dateOfBirth.Day <= 20) ? "Козоріг" : "Водолій";
+                        break;
+                    case 2:
+                        sign = (_dateOfBirth.Day <= 19) ? "Водолій" : "Риби";
+                        break;
+                    case 3:
+                        sign = (_dateOfBirth.Day <= 21) ? "Риби" : "Овен";
+                        break;
+                    case 4:
+                        sign = (_dateOfBirth.Day <= 20) ? "Овен" : "Телець";
+                        break;
+                    case 5:
+                        sign = (_dateOfBirth.Day <= 21) ? "Телець" : "Близнюки";
+                        break;
+                    case 6:
+                        sign = (_dateOfBirth.Day <= 22) ? "Близнюки" : "Рак";
+                        break;
+                    case 7:
+                        sign = (_dateOfBirth.Day <= 23) ? "Рак" : "Лев";
+                        break;
+                    case 8:
+                        sign = (_dateOfBirth.Day <= 23) ? "Лев" : "Діва";
+                        break;
+                    case 9:
+                        sign = (_dateOfBirth.Day <= 24) ? "Діва" : "Терези";
+                        break;
+                    case 10:
+                        sign = (_dateOfBirth.Day <= 23) ? "Терези" : "Скорпіон";
+                        break;
+                    case 11:
+                        sign = (_dateOfBirth.Day <= 23) ? "Скорпіон" : "Стрілець";
+                        break;
+                    case 12:
+                        sign = (_dateOfBirth.Day <= 22) ? "Стрілець" : "Козоріг";
+                        break;
+                }
+
+                return sign;
+            }
+        }
+
+        public string ChineseSign
+        {
+            //find out chinese zodiac sign
+            get {
+                string sign = null;
+                string[] signs =
+                {
+                    "Щур", "Бик", "Тигр", "Кролик", "Дракон", "Змія", "Кінь", "Коза", "Мавпа", "Півень", "Собака",
+                    "Свиня"
+                };
+                return signs[(_dateOfBirth.Year - 4) % 12];
+            }
+        }
+
+        public bool IsBirthday
+        {
+            //check whether today is person's birthday
+            get
+            {
+                if (_dateOfBirth.Month == DateTime.Now.Month && _dateOfBirth.Day == DateTime.Now.Day) return true;
+                return false;
+            }
+        }
+
+        #endregion
+
+        #region Methods
+        public int Age()
+        {
+            int age = -1;
+            if (_dateOfBirth <= DateTime.Now)
+            {
+                age = DateTime.Now.Year - _dateOfBirth.Year;
+                if (_dateOfBirth.DayOfYear > DateTime.Now.DayOfYear)
+                {
+                    age--;
+                }
+            }
+            return age;
+        }
+
+
+        public bool IsValid()
+        {
+            //check whether person's age is appropriate 
+            if (Age() < 0 || Age() > 135) return false;
+            return true;
+        }
+
+        #endregion
+    }
+}
--- ViewModels/MainViewModel.cs
+++ ViewModels/MainViewModel.cs
@@ -3,6 +3,7 @@ using System.Collections.Generic;
 using System.ComponentModel;
 using System.Linq;
 using System.Text;
+using System.Threading;
 using System.Threading.Tasks;
 using System.Windows;
 using Practice_2.Models;
@@ -13,15 +14,66 @@ namespace Practice_2.ViewModels
     class MainViewModel : INotifyPropertyChanged
     {
         #region Fields
-        private User _user = new User();
+        private Person _user=new Person(null,null,null);
         private RelayCommand<object> _selectDateCommand;
+        private bool _isEnabled=true;
         #endregion
 
         #region Properties
+        public string Name
+        {
+            get
+            {
+                if (_user.IsValid())
+                {
+                    return _user.Name;
+                }
+
+                return "---";
+            }
+            set { _user.Name = value; }
+        }
+
+        public string Surname
+        {
+            get
+            {
+                if (_user.IsValid())
+                {
+                    return _user.Surname;
+                }
+
+                return "---";
+            }
+            set { _user.Surname = value; }
+        }
+
+        public string Email
+        {
+            get
+            {
+                if (_user.IsValid())
+                {
+                    return _user.Email;
+                }
+
+                return "---";
+            }
+            set { _user.Email = value; }
+        }
+
         public DateTime DateOfBirth
         {
             get { return _user.DateOfBirth; }
-            set { _user.DateOfBirth = value; }
+            set
+            {
+                _user.DateOfBirth = value;
+                IsEnabled = false;
+                NotifyPropertyChanged("IsEnabled");
+                Task.Run(async () => await renewData());
+                IsEnabled = true;
+                NotifyPropertyChanged("IsEnabled");
+            }
         }
 
         public string Age
@@ -38,38 +90,102 @@ namespace Practice_2.ViewModels
             set { _ = _user.Age(); }
         }
 
+        public string IsAdult
+        {
+            get
+            {
+                if(_user.IsValid())
+                    if (_user.IsAdult)
+                    {
+                        return "Так";
+                    }
+                    else return "Ні";
+
+                return "---";
+            }
+            set{}
+        }
+
+        public string IsBirthday
+        {
+            get
+            {
+                if (_user.IsValid())
+                    if (_user.IsBirthday)
+                    {
+                        return "Так";
+                    }
+                    else return "Ні";
+
+                return "---";
+            }
+            set{}
+        }
+
         public string WesternZodiacSign
         {
-            get { return _user.WesternZodiacSign(); }
-            set { _ = _user.WesternZodiacSign(); }
+            get
+            {
+                if (_user.IsValid())
+                {
+                    return _user.SunSign;
+                }
+
+                return "---";
+            }
+            set { _ = _user.SunSign; }
         }
         public string ChineseZodiacSign
         {
             get
-            { return _user.ChineseZodiacSign(); }
-            set { _ = _user.ChineseZodiacSign(); }
+            {
+                Thread.Sleep(5000);
+                if (_user.IsValid())
+                {
+                    return _user.ChineseSign;
+                }
+
+                return "---";
+            }
+            set { _ = _user.ChineseSign; }
         }
 
+        public bool IsEnabled
+        {
+            get
+            {
+                return _isEnabled;
+            }
+            set
+            {
+                _isEnabled = value;
+            }
+        }
         public RelayCommand<object> SelectDateCommand
         {
             get
             {
-                return _selectDateCommand ??= new RelayCommand<object>(_ => SetData());
+                return _selectDateCommand ??= new RelayCommand<object>(_ => SetData(), CanExecute);
             }
         }
         #endregion
         private void SetData()
         {
+            NotifyPropertyChanged("Name");
+            NotifyPropertyChanged("Surname");
+            NotifyPropertyChanged("Email");
             NotifyPropertyChanged("Age");
             NotifyPropertyChanged("WesternZodiacSign");
             NotifyPropertyChanged("ChineseZodiacSign");
+            NotifyPropertyChanged("IsAdult");
+            NotifyPropertyChanged("IsBirthday");
             if (!_user.IsValid())
             {
                 MessageBox.Show("Некоректно обрано дату народження!");
             }
             else
             {
-                if (_user.IsBirthday())
+                if (_user.IsBirthday)
                 {
                     MessageBox.Show("З Днем народження!");
                 }
@@ -77,10 +193,22 @@ namespace Practice_2.ViewModels
             }
         }
 
+        private async Task renewData()
+        {
+            await Task.Run(() => Age);
+            await Task.Run(() => WesternZodiacSign);
+            await Task.Run(() => ChineseZodiacSign);
+        }
+
         public event PropertyChangedEventHandler PropertyChanged;
         protected void NotifyPropertyChanged(string info)
         {
             PropertyChanged?.Invoke(this, new PropertyChangedEventArgs(info));
+        }
+
+        private bool CanExecute(object obj)
+        {
+            return !String.IsNullOrWhiteSpace(_user.Name) && !String.IsNullOrWhiteSpace(_user.Surname)&&!String.IsNullOrWhiteSpace(_user.Email);
         }
 
 
